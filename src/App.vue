c741aa37ab733828617de3bcc517f33694da7e80
ahmdali25 <ahmadaliazhary@gmail.com>
2023-07-16 21:31:53 +0700

    ADD - add footer component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import TheNavbar from './components/TheNavbar.vue';
 import TheSidebar from './components/TheSidebar.vue';
+import TheFooter from './components/TheFooter.vue';
 import { RouterView } from 'vue-router';
 </script>
 
@@ -8,6 +9,7 @@ import { RouterView } from 'vue-router';
   <TheNavbar/>
   <TheSidebar/>
   <RouterView/>
+  <TheFooter/>
 </template>
 
 <style scoped>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <p>
+        <strong>&copy; 2023 Admin UI</strong> by <a href="https://github.com/ahmdali25" target="_blank">Ahmad Ali Azhary</a>.
+      </p>
+    </div>
+  </footer>
+</template>
+
+<style>
+.footer {
+	background-color: #FFFFFF;
+	padding: 1.5rem;
+}
+</style>
